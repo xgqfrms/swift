f7a4253137162c5e06b182d15ed315ddd830a191
xgqfrms <xgqfrms@outlook.com>
2020-01-20 15:10:26 +0800

    Create WKWebView-ViewController.swift

---
+++ WKWebView/WKWebView-ViewController.swift
@@ -0,0 +1,21 @@
+
+import UIKit
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"https://www.apple.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+}
