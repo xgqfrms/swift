8cc742e777fdfa71dc09ca77a46882677eb9af96
xgqfrms <xgqfrms@outlook.com>
2020-01-20 15:54:59 +0800

    Create wkwebview-playground.swift

---
+++ WKWebView/wkwebview-playground.swift
@@ -0,0 +1,50 @@
+//: A UIKit based Playground for presenting user interface
+
+
+// PlaygroundSupport
+import PlaygroundSupport
+
+import UIKit
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string: "https://cdn.xgqfrms.xyz")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+}
+
+PlaygroundPage.current.liveView = ViewController();
+
+/*
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "Hello World!"
+        label.textColor = .black
+        
+        view.addSubview(label)
+        self.view = view
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
